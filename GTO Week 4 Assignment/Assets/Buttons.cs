bb680a9a80f6b37c5b0b9e70656124fc4b4d12dc
Lieuw, Sean S.K <s.lieuw@student.fontys.nl>
2018-03-08 14:08:26 +0100

    GTO Week 4 Assignment

---
+++ GTO Week 4 Assignment/Assets/Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Buttons : MonoBehaviour {
+
+    public Text myText;
+    // Use this for initialization
+    public void increaseNumber()
+    {
+        int number = Convert.ToInt32(myText.text);
+
+        number = number +10;
+        myText.text = number.ToString();
+    }
+    public void decreaseNumber()
+    {
+        int number = Convert.ToInt32(myText.text);
+        number = number -10;
+
+        myText.text = number.ToString();
+    }
+}
