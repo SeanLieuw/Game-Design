9ab375feef1956450d17633bb6a88dbb4751e979
Lieuw, Sean S.K <laokaishun@gmail.com>
2018-03-09 23:49:13 +0100

    Grid & Units

---
+++ Grid & Units/Assets/Grid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Grid : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Tile prefab;
+    // Use this for initialization
+    public Unit prefab2;
+    void Start()
+    {
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                Instantiate(prefab, new Vector3(x * 1, 0, y * 1), Quaternion.identity);
+            }
+
+        }
+        for (int x = 0; x < 9; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                Instantiate(prefab2, new Vector3(x * 1, 0, y * 1), Quaternion.identity);
+                y++;
+            }
+            x++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
